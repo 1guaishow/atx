6c228b2ddaad6169fca193b267e90ee906531793
guaishow <yyl310@gmail.com>
2024-06-02 21:36:14 +0800

    完善

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ curl -o /usr/bin/restart https://cdn.jsdelivr.net/gh/1guaishow/atx@main/restart
 curl -o /usr/bin/sensor https://cdn.jsdelivr.net/gh/1guaishow/atx@main/sensor
 sudo mv /etc/kvmd/override.yaml /etc/kvmd/override.yaml.bak
 curl -o /etc/kvmd/override.yaml https://cdn.jsdelivr.net/gh/1guaishow/atx@main/override.yaml
-curl -o install https://cdn.jsdelivr.net/gh/1guaishow/atx@main/setup
+curl -o setup https://cdn.jsdelivr.net/gh/1guaishow/atx@main/setup
 echo "kvmd ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/custom_commands > /dev/null
 echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", SYMLINK+="KVMD_ATX"' | sudo tee /etc/udev/rules.d/99-kvmd.rules > /dev/null
 chmod +x setup
